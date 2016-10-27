a31263535b75a5d3d3601b007cf028bcf5489612
ColdNightHour <jdixo001@ucr.edu>
2016-10-27 12:48:33 -0700

    Works without active processes of me on my laptop

--- src/main/java/Part1/Main.java
+++ src/main/java/Part1/Main.java
@@ -20,8 +20,8 @@ public class Main{
     public static final String baseFileName = "TWEET_FILE";
     public static int tweetCnt = 0; //COUNTING # OF TWEETS SEEN
     public static int tweetByteCnt = 0;
-    public static final long MAX_TWEET_CNT = 1000000000L; //CAP FOR DETECTING DUPES
-    public static final long MAX_BYTE_CNT = 26843545600L; //Each tweet isa bout 200 Bytes, we want files of about 25 MB, 150000000 tweets is about 25 MB
+    public static final int MAX_TWEET_CNT = 15000000; //CAP FOR DETECTING DUPES
+    public static final int MAX_BYTE_CNT = 10485760/10; //NUMBER OF BYTES FOR 10MB FILE 10485760      1048576
     /*Files of size 150000000*/
     public static HashSet<Long> tweetIdHash = new HashSet<Long>();
     public static File currentFile; //CURRENT FILE BEING I/O'd TO
@@ -29,14 +29,23 @@ public class Main{
     public static String location = "NULL";
     public static Boolean run = true;
     public static StringBuilder buffer = new StringBuilder("");
-    public static int fileCnt = 1;
+    public static int fileCnt = 25;
+    public static int filePassCnt = 7;
+    public static PrintWriter writer;
     public static void main(String [] args) {
         ConfigurationBuilder config = new ConfigurationBuilder();
         config.setOAuthConsumerKey("K8P38gSDQUTk1Et7QAxDg5a1B")
             .setOAuthConsumerSecret("GcRmcdSe6CbLT1BvGNZREso8jrcNRENsYDc4crHWr4UOvaA44s")
             .setOAuthAccessToken("1853291412-qwBGaZr2S64KfmIU0hB1aYi4Kc0goxOQWzyeA20")
             .setOAuthAccessTokenSecret("CV17evpidxeGGTkP6mn7OwOOMIflt6DZSljwO0jbq6wZM");
-
+        /*try{
+           writer = new PrintWriter("./tweets/file" + fileCnt + ".txt", "UTF-8");
+        } catch(FileNotFoundException e) {
+          System.out.println("NO FILE");
+        }
+        catch(UnsupportedEncodingException e) {
+          System.out.println("WRONG ENCODING");
+        }*/
         TwitterStream twitterStream = new TwitterStreamFactory(config.build()).getInstance();
         StatusListener statusListener = new StatusListener() {
             public void onStatus(Status status) {
@@ -44,16 +53,15 @@ public class Main{
 
                   //DUPLICATE DETECTION
                   long id = status.getId();
-                  int int_id = (int) id;
                   if (tweetCnt < MAX_TWEET_CNT) {
                       ++tweetCnt;
                       if (tweetIdHash.contains(id)) {
-                          System.out.println("DUPLICATE");
                           return;
                       } else {
                           tweetIdHash.add(id);
                       }
                   } else {
+                      System.out.println("Cap Reset for duplicates");
                       tweetCnt = 1;
                       tweetIdHash.clear();
                       tweetIdHash.add(id);
@@ -77,14 +85,6 @@ public class Main{
                       URL url = new URL(item);
                       org.json.JSONObject urlBundle = new JSONObject();
                       urlBundle.put("url", url);
-                      // If possible then replace with anchor...
-                      try {
-                          urlBundle.put("url_title", Jsoup.connect(url.toString()).get().title()); //TODO: FUCKING DO THIS
-                      } catch (IOException e) {
-                          urlBundle.put("url_title", "Could not parse title");
-                      } catch(IllegalArgumentException e) {
-                        urlBundle.put("url_title", "Exception for illegal argument");
-                      }
                       urlArray.put(urlBundle);
                   } catch (MalformedURLException e) {
                       // If there was an URL that was not it!...
@@ -96,21 +96,35 @@ public class Main{
                   else
                     object.put("url_list", "NULL");
                   String jsonString = object.toString() + "\n";
-
-                  tweetByteCnt+=jsonString.getBytes().length;
+                  try{
+                    tweetByteCnt+=jsonString.getBytes("UTF-8").length;
+                  } catch(UnsupportedEncodingException e) {
+                    System.out.println("WRONG ENCODING");
+                  }
                   buffer.append(jsonString);
                   if(tweetByteCnt >= MAX_BYTE_CNT) {
                     tweetByteCnt = 0;
                     try{
-                      PrintWriter writer = new PrintWriter("./tweets/file" + fileCnt + ".txt", "UTF-8");
-                      System.out.println("FILE WRITTEN");
-                      writer.println(buffer);
+                      System.out.println("FILE " + fileCnt + " WRITTEN OF PASS " + filePassCnt);
+                      filePassCnt++;
+                      if(filePassCnt == 1) {
+                        fileCnt++;
+                        writer = new PrintWriter("./tweets/file" + fileCnt + ".txt", "UTF-8");
+                        writer.println(buffer);
+                      }
+                      else if(filePassCnt != 1) {
+                        writer= new PrintWriter(new FileOutputStream("./tweets/file" + fileCnt + ".txt", true));
+                        writer.append(buffer);
+                      }
+                      if(filePassCnt == 11)
+                        filePassCnt = 0;
                       writer.close();
-                      fileCnt++;
-                      buffer.setLength(0);
+                      buffer = new StringBuilder();
+                      buffer.trimToSize();
+                      System.gc();
                     }
                     catch(FileNotFoundException e) {
-
+                      System.out.println("NO FILE");
                     }
                     catch(UnsupportedEncodingException e) {
 
@@ -140,7 +154,7 @@ public class Main{
             }
 
             public void onStallWarning(StallWarning warning) {
-                System.out.println("Got stall warning:" + warning);
+                System.out.println("Got stall warning");
             }
 
             public void onException(Exception ex) {
@@ -149,16 +163,17 @@ public class Main{
         };
 
         twitterStream.addListener(statusListener);
-        /*
-        currentFile = new File(baseFileName + Integer.toString(fileCnt));
-        try {
-            currentFile.createNewFile();
-            stream = new OutputStreamWriter(new FileOutputStream(currentFile), "UTF8");
-        } catch (Exception e) {
-            System.out.println("CREATING FILE WENT WRONG");
-            return;
-        }
-        */
+
         twitterStream.sample();
     }
 }
+/*URL PROCESS TITLE CODE
+// If possible then replace with anchor...
+/*try {
+    urlBundle.put("url_title", Jsoup.connect(url.toString()).get().title()); //TODO: FUCKING DO THIS
+} catch (IOException e) {
+    urlBundle.put("url_title", "Could not parse title");
+} catch(IllegalArgumentException e) {
+  urlBundle.put("url_title", "Exception for illegal argument");
+}
+*/
