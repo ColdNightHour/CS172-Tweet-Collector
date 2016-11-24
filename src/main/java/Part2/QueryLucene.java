712f110c2e1d057a1f505a92425585632a4303b7
ColdNightHour <jdixo001@ucr.edu>
2016-11-23 19:50:34 -0800

    Added query stuff

--- src/main/java/Part2/QueryLucene.java
+++ src/main/java/Part2/QueryLucene.java
@@ -11,6 +11,7 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.search.IndexSearcher;
@@ -29,25 +30,40 @@ import org.nibor.autolink.LinkType;
 import java.io.*;
 import java.util.EnumSet;
 import java.util.NoSuchElementException;
-/**
- * Created by coldnighthour on 11/20/16.
- */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+
 public class QueryLucene {
   public static void main(String [] args) throws IOException{
     File indexDir = new File("./LuceneIndex");
     indexDir.mkdir();
     Directory d = FSDirectory.open(indexDir.toPath());
     StandardAnalyzer analyzer = new StandardAnalyzer();
-    //String querystr = /*args.length > 0 ? args[0] : "lucene"*/messga "bomb";
-
+    Map<String, Float> boosts = new HashMap<String, Float>();
+    boosts.put("message", (float)(10.0));
     try{
-      Query q = new QueryParser("message", analyzer).parse("message");
+      //System.out.println(args[0]);
+      Query q = new MultiFieldQueryParser(new String[] {"name", "hashtags", "message", "url_titles", "timestamp", "location"}, analyzer, boosts).parse("this is it");
       int hitsPerPage = 10;
       IndexReader reader = DirectoryReader.open(d);
       IndexSearcher searcher = new IndexSearcher(reader);
       TopDocs docs = searcher.search(q, hitsPerPage);
       ScoreDoc[] hits = docs.scoreDocs;
-      System.out.println("Found " + hits.length + " hits.");
+      System.out.println("~HITS~");
+      for(int i = 0; i < hits.length; ++i) {
+        int docId = hits[i].doc;
+        Document de = searcher.doc(docId);
+        System.out.println("DOC-BEG");
+        System.out.print("{{\"score\" : " + hits[i].score + "}, ");
+        System.out.print("{\"name\" : \"" + de.get("name") + "\"}, ");
+        System.out.print("{\"message\" : \"" + de.get("message") + "\"}, ");
+        System.out.print("{\"hashtags\" : \"" + de.get("hashtags") + "}, ");
+        System.out.print("{\"location\" : \"" + de.get("location") + "\"}, ");
+        System.out.print("{\"url_titles\" : " + de.get("url_titles") + "}, ");
+        System.out.println("{\"timestamp\" : \"" + de.get("timestamp") + "\"}}");
+      }
     } catch (ParseException e){
 
     }
